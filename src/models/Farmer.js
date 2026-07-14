a811bea740c67faba3618daae6b562dfa7cba351
haddiouiikram6-beep <haddiouiikram6@gmail.com>
2026-07-14 13:55:27 +0200

    create model -farmer.js-

---
+++ src/models/Farmer.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Farmer = sequelize.define(
+  "Farmer",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "farmers",
+    timestamps: true,
+  }
+);
+
+module.exports = Farmer;
