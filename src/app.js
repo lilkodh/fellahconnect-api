479ab3ebdae2b3c04d2f227baee0e171932930d9
miftahsara390-coder <miftahsara390@gmail.com>
2026-07-15 13:53:42 +0200

    Connect product market and market price routes in app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+
 app.use(express.json());
 
 
+const marketRoutes = require("./routes/market.routes");
+const marketPriceRoutes = require("./routes/marketPrice.routes");
+const productRoutes = require("./routes/product.routes");
 
+app.use("/markets", marketRoutes);
+app.use("/market-prices", marketPriceRoutes);
+app.use("/products", productRoutes);
 
-
-
-module.exports = app ;
+module.exports = app;
