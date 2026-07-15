7c38325d45a10adcbf3923367c2361c5d7c678e4
lilkodh <khaliddrihemdev@gmail.com>
2026-07-15 13:30:10 +0100

    merge: integrate farmer and product modules into dev

--- src/models/index.js
+++ src/models/index.js
@@ -1,18 +1,28 @@
-Market.hasMany(MarketPrice,{
- foreignKey:"marketId"
+const sequelize = require("../config/database");
+const { DataTypes } = require("sequelize");
+
+const Market = require("./market")(sequelize, DataTypes);
+const MarketPrice = require("./marketPrice")(sequelize, DataTypes);
+const Product = require("./product")(sequelize, DataTypes);
+
+Market.hasMany(MarketPrice, {
+  foreignKey: "marketId",
 });
 
-
-MarketPrice.belongsTo(Market,{
- foreignKey:"marketId"
+MarketPrice.belongsTo(Market, {
+  foreignKey: "marketId",
 });
 
-
-Product.hasMany(MarketPrice,{
- foreignKey:"productId"
+Product.hasMany(MarketPrice, {
+  foreignKey: "productId",
 });
 
-
-MarketPrice.belongsTo(Product,{
- foreignKey:"productId"
+MarketPrice.belongsTo(Product, {
+  foreignKey: "productId",
 });
+
+module.exports = {
+  Market,
+  MarketPrice,
+  Product,
+};
